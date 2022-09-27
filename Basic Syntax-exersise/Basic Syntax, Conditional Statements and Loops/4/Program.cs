807a5370e6740c23f7d3cc4ee9cf329d53f28a7e
KrasimirKolew <krasirumenov789@gmail.com>
2022-09-27 18:29:41 +0300

    Add exersice

---
+++ Basic Syntax-exersise/Basic Syntax, Conditional Statements and Loops/4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine())+30;
+
+            if (min>59)
+            {
+                hour += 1;
+                min -= 60;
+
+            }
+            if (hour>23)
+            {
+                hour = 0;
+            }
+            Console.WriteLine($"{hour}:{min:d2}");
+        }
+    }
+}
---
+++ Basic Syntax-exersise/Basic Syntax, Conditional Statements and Loops/6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string island = Console.ReadLine();
+
+            switch (island)
+            {
+                case "USA":
+                    Console.WriteLine("English");
+                    break;
+
+                case "England":
+                    Console.WriteLine("English");
+                    break;
+
+                case "Spain":
+                    Console.WriteLine("Spanish");
+                    break;
+
+                case "Argentina":
+                    Console.WriteLine("Spanish");
+                    break;
+
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+
+                    default: 
+                    Console.WriteLine("unknown");
+                    break;
+
+
+
+
+
+            }
+
+
+
+
+        }
+    }
+}
